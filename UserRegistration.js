a58b419a74a4edabfc02c5b25e7b3b2a614c1607
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-10-29 01:45:23 +0530

    add: Implement Regular expression for Email Address

--- UserRegistration.js
+++ UserRegistration.js
@@ -1,10 +1,31 @@
-let NAME_PATTERN = new RegExp('^[A-Z]{1}[a-z]{2,}$');
+//Constants
+const NAME = "Name";
+const EMAIL = "Email";
+//Variables
+let namePattern = new RegExp('^[A-Z]{1}[a-z]{2,}$');
+let emailPattern = new RegExp('^[a-zA-Z0-9]+([._+#][a-zA-Z0-9]+)*@[a-zA-Z1-9]+.[a-zA-Z]{2,4}(.[a-zA-Z]{2,3})?$');
 let pattern ;
-function validate(pattern) {
-    return NAME_PATTERN.test(pattern);
+let check ;
+//Function Validate Name
+validateName = (pattern) =>  {
+    return namePattern.test(pattern);
 }
-function checkPattern(pattern) {
-    var check = validate(pattern); 
+//Function Validate Email
+validateEmail = (pattern) => {
+    return  emailPattern.test(pattern);
+}
+
+checkType = (type,pattern) => {
+    switch(type){
+        case "Name" :
+            return validateName(pattern);
+        case "Email" :
+            return validateEmail(pattern);
+    }
+}
+
+function checkPattern(type,pattern) {
+     check = checkType(type,pattern); 
     if(check == true){
         console.log("Regex Approved");
     }
@@ -12,5 +33,7 @@ function checkPattern(pattern) {
         console.log("Pattern Rejected");
     }
 }
-checkPattern("Saurabh");
-checkPattern("Dagwar");
+checkPattern(NAME,"Saurabh");
+checkPattern(NAME,"Dagwar");
+checkPattern(EMAIL,"s@gmail.com")
+
