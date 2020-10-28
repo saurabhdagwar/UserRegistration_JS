07d02d9d7162f1bef563cf7ab973909bebb9e910
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-10-29 02:26:49 +0530

    add: Initialize Regex for minimum 8 digit password pattern

--- UserRegistration.js
+++ UserRegistration.js
@@ -2,10 +2,12 @@
 const NAME = "Name";
 const EMAIL = "Email";
 const MOBILE = "Mobile";
+const PASSWORD  = "Password";
 //Variables
 let namePattern = new RegExp('^[A-Z]{1}[a-z]{2,}$');
 let emailPattern = new RegExp('^[a-zA-Z0-9]+([._+#][a-zA-Z0-9]+)*@[a-zA-Z1-9]+.[a-zA-Z]{2,4}(.[a-zA-Z]{2,3})?$');
-let mobilePattern = new RegExp('(^[0-9]{1,3}[ ]+)?[6-9]+[0-9]{9,9}$');
+let mobilePattern = new RegExp('(^[0-9]{1,3}[ ]*)?[6-9]+[0-9]{9,9}$');
+let passwordPattern = new RegExp('^[0-9A-Za-z!@#$$%^&*]{8,}$');
 let pattern ;
 let check ;
 //Function Validate Name
@@ -20,6 +22,10 @@ validateEmail = (pattern) => {
 validateMobile = (pattern) => {
     return mobilePattern.test(pattern);
 }
+//function Password Pattern
+validatePassword = (pattern) => {
+    return passwordPattern.test(pattern);
+}
 
 checkType = (type,pattern) => {
     switch(type){
@@ -29,20 +35,23 @@ checkType = (type,pattern) => {
             return validateEmail(pattern);
         case MOBILE :
             return validateMobile(pattern);
+        case PASSWORD :
+            return validatePassword(pattern);
     }
 }
 
 function checkPattern(type,pattern) {
      check = checkType(type,pattern); 
     if(check == true){
-        console.log("Regex Approved");
+        return "Regex Approved";
     }
     else{
-        console.log("Pattern Rejected");
+        return "Pattern Rejected";
     }
 }
-checkPattern(NAME,"Saurabh");
-checkPattern(NAME,"Dagwar");
-checkPattern(EMAIL,"saurabhdagwar@gmail.com");
-checkPattern(MOBILE,"91 8856852313");
+console.log(`Validate First Name: ${checkPattern(NAME,"Saurabh")}`); 
+console.log(`Validate Last Name: ${checkPattern(NAME,"Dagwar")}`);
+console.log(`Validate Email: ${checkPattern(EMAIL,"saurabhdagwar@gmail.com")}`) ;
+console.log(`Validate Mobile Number: ${checkPattern(MOBILE,"91 8856852313")}`) ;
+console.log(`Validate Password: ${checkPattern(PASSWORD,"saurabhdagwar")}`) ;
 
