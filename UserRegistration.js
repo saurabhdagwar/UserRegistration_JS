8abfb2417bc8582bc4416c152eebdd0550f99cad
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-10-29 02:05:21 +0530

    add: Validate mobile number pattern

--- UserRegistration.js
+++ UserRegistration.js
@@ -1,9 +1,11 @@
 //Constants
 const NAME = "Name";
 const EMAIL = "Email";
+const MOBILE = "Mobile";
 //Variables
 let namePattern = new RegExp('^[A-Z]{1}[a-z]{2,}$');
 let emailPattern = new RegExp('^[a-zA-Z0-9]+([._+#][a-zA-Z0-9]+)*@[a-zA-Z1-9]+.[a-zA-Z]{2,4}(.[a-zA-Z]{2,3})?$');
+let mobilePattern = new RegExp('(^[0-9]{1,3}[ ]+)?[6-9]+[0-9]{9,9}$');
 let pattern ;
 let check ;
 //Function Validate Name
@@ -14,13 +16,19 @@ validateName = (pattern) =>  {
 validateEmail = (pattern) => {
     return  emailPattern.test(pattern);
 }
+//function validate Mobile Number
+validateMobile = (pattern) => {
+    return mobilePattern.test(pattern);
+}
 
 checkType = (type,pattern) => {
     switch(type){
-        case "Name" :
+        case NAME :
             return validateName(pattern);
-        case "Email" :
+        case EMAIL :
             return validateEmail(pattern);
+        case MOBILE :
+            return validateMobile(pattern);
     }
 }
 
@@ -35,5 +43,6 @@ function checkPattern(type,pattern) {
 }
 checkPattern(NAME,"Saurabh");
 checkPattern(NAME,"Dagwar");
-checkPattern(EMAIL,"s@gmail.com")
+checkPattern(EMAIL,"saurabhdagwar@gmail.com");
+checkPattern(MOBILE,"91 8856852313");
 
