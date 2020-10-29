73dacedcc25a07646325fc6f42dc4c4ed5ee3703
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-10-29 09:02:30 +0530

    refactor: Vadidate User details like name, email, mobile and password

--- UserRegistration.js
+++ UserRegistration.js
@@ -2,56 +2,60 @@
 const NAME = "Name";
 const EMAIL = "Email";
 const MOBILE = "Mobile";
-const PASSWORD  = "Password";
+const PASSWORD = "Password";
 //Variables
-let namePattern = new RegExp('^[A-Z]{1}[a-z]{2,}$');
-let emailPattern = new RegExp('^[a-zA-Z0-9]+([._+#][a-zA-Z0-9]+)*@[a-zA-Z1-9]+.[a-zA-Z]{2,4}(.[a-zA-Z]{2,3})?$');
-let mobilePattern = new RegExp('(^[0-9]{1,3}[ ]*)?[6-9]+[0-9]{9,9}$');
-let passwordPattern = new RegExp('^(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%*&+?/!]{1}).{8,}$');
-let pattern ;
-let check ;
+let namePattern = new RegExp("^[A-Z]{1}[a-z]{2,}$");
+let emailPattern = new RegExp(
+  "^[a-zA-Z0-9]+([._+#][a-zA-Z0-9]+)*@[a-zA-Z1-9]+.[a-zA-Z]{2,4}(.[a-zA-Z]{2,3})?$"
+);
+let mobilePattern = new RegExp("(^[0-9]{1,3}[ ]*)?[6-9]+[0-9]{9,9}$");
+let passwordPattern = new RegExp(
+  "^(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%*&+?/!]{1}).{8,}$"
+);
+let pattern;
+let check;
 //Function Validate Name
-validateName = (pattern) =>  {
-    return namePattern.test(pattern);
-}
+validateName = (pattern) => {
+  return namePattern.test(pattern);
+};
 //Function Validate Email
 validateEmail = (pattern) => {
-    return  emailPattern.test(pattern);
-}
+  return emailPattern.test(pattern);
+};
 //function validate Mobile Number
 validateMobile = (pattern) => {
-    return mobilePattern.test(pattern);
-}
+  return mobilePattern.test(pattern);
+};
 //function Password Pattern
 validatePassword = (pattern) => {
-    return passwordPattern.test(pattern);
-}
+  return passwordPattern.test(pattern);
+};
 
-checkType = (type,pattern) => {
-    switch(type){
-        case NAME :
-            return validateName(pattern);
-        case EMAIL :
-            return validateEmail(pattern);
-        case MOBILE :
-            return validateMobile(pattern);
-        case PASSWORD :
-            return validatePassword(pattern);
-    }
-}
+checkType = (type, pattern) => {
+  switch (type) {
+    case NAME:
+      return validateName(pattern);
+    case EMAIL:
+      return validateEmail(pattern);
+    case MOBILE:
+      return validateMobile(pattern);
+    case PASSWORD:
+      return validatePassword(pattern);
+  }
+};
 
-function checkPattern(type,pattern) {
-     check = checkType(type,pattern); 
-    if(check == true){
-        return "Regex Approved";
-    }
-    else{
-        return "Pattern Rejected";
-    }
+function checkPattern(type, pattern) {
+  check = checkType(type, pattern);
+  if (check == true) {
+    return "Regex Approved";
+  } else {
+    return "Pattern Rejected";
+  }
 }
-console.log(`Validate First Name: ${checkPattern(NAME,"Saurabh")}`); 
-console.log(`Validate Last Name: ${checkPattern(NAME,"Dagwar")}`);
-console.log(`Validate Email: ${checkPattern(EMAIL,"saurabhdagwar@gmail.com")}`) ;
-console.log(`Validate Mobile Number: ${checkPattern(MOBILE,"91 8856852313")}`) ;
-console.log(`Validate Password: ${checkPattern(PASSWORD,"Saurabdagwar@1")}`) ;
-
+console.log(`Validate First Name: ${checkPattern(NAME, "Saurabh")}`);
+console.log(`Validate Last Name: ${checkPattern(NAME, "Dagwar")}`);
+console.log(
+  `Validate Email: ${checkPattern(EMAIL, "saurabhdagwar@gmail.com")}`
+);
+console.log(`Validate Mobile Number: ${checkPattern(MOBILE, "91 8856852313")}`);
+console.log(`Validate Password: ${checkPattern(PASSWORD, "Saurabdagwar@1")}`);
